2a382c85a07251e5295807a32309b6b2ba5e858f
Antónia Brito <102589208+Nia3324@users.noreply.github.com>
2023-07-08 20:57:48 +0100

    Delete AI_15Puzzle_2223 directory

--- AI_15Puzzle_2223/AStarStateComparator.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class AStarStateComparator implements Comparator<AStarState>{
-    @Override
-    public int compare(AStarState o1, AStarState o2) {
-        return o1.getBoardObject().toString().compareTo(o2.getBoardObject().toString());
-    }
-}
